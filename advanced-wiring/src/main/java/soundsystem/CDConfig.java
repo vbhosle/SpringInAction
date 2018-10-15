4f068874f250d1dd2faccf9986791456a06b49c6
Viraj Bhosle <bhosleviraj@gmail.com>
2018-10-15 15:11:28 +0530

    Writing a simple controller

--- advanced-wiring/src/main/java/soundsystem/CDConfig.java
+++ advanced-wiring/src/main/java/soundsystem/CDConfig.java
@@ -1,9 +1,7 @@
 package soundsystem;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import soundsystem.disc.CompactDisc;
 
 @Configuration
 public class CDConfig {
---
+++ spittr/src/main/java/spittr/config/WebConfig.java
@@ -0,0 +1,28 @@
+package spittr.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("spittr.web")
+public class WebConfig implements WebMvcConfigurer {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+		resolver.setExposeContextBeansAsAttributes(true);
+		return resolver;
+	}
+
+	//forward requests for static resources to the servlet container’s default servlet
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+}
