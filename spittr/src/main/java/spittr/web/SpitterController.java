0d54442a7f193c0fe8bc192e1fba203a1f0a6e1c
Viraj Bhosle <bhosleviraj@gmail.com>
2018-11-20 18:31:50 +0530

    Chapter 5: Form Validation

--- spittr/src/main/java/spittr/web/SpitterController.java
+++ spittr/src/main/java/spittr/web/SpitterController.java
@@ -1,7 +1,10 @@
 package spittr.web;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,7 +28,10 @@ public class SpitterController {
 	}
 	
 	@RequestMapping(value="register", method= RequestMethod.POST)
-	public String processRegistration(Spitter spitter) {
+	public String processRegistration(@Valid Spitter spitter, Errors errors) {
+		if(errors.hasErrors()) {
+			return "registerForm";
+		}
 		spitterRepository.save(spitter);
 		
 		return "redirect:/spitter/" + spitter.getUsername();
