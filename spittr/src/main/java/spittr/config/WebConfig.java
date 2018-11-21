ba27d9884290c0d61b038833dbf5653bd2bdcf25
Viraj Bhosle <bhosleviraj@gmail.com>
2018-11-21 14:43:55 +0530

    Configuring a Tiles view resolver

--- spittr/src/main/java/spittr/config/WebConfig.java
+++ spittr/src/main/java/spittr/config/WebConfig.java
@@ -10,19 +10,21 @@ import org.springframework.web.servlet.config.annotation.DefaultServletHandlerCo
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("spittr.web")
 public class WebConfig implements WebMvcConfigurer {
+//
+//	@Bean
+//	public ViewResolver viewResolver() {
+//		InternalResourceViewResolver resolver = new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+//		resolver.setExposeContextBeansAsAttributes(true);
+//		return resolver;
+//	}
 
-	@Bean
-	public ViewResolver viewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
-		resolver.setExposeContextBeansAsAttributes(true);
-		return resolver;
-	}
-	
 	@Bean
 	public MessageSource messageSource() {
 		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
@@ -30,7 +32,21 @@ public class WebConfig implements WebMvcConfigurer {
 		return messageSource;
 	}
 
-	//forward requests for static resources to the servlet container’s default servlet
+	@Bean
+	public TilesConfigurer tilesConfigurer() {
+		TilesConfigurer tiles = new TilesConfigurer();
+		tiles.setDefinitions(new String[] { "/WEB-INF/layout/tiles.xml" });
+		tiles.setCheckRefresh(true);
+		return tiles;
+	}
+
+	@Bean
+	public ViewResolver viewResolver() {
+		return new TilesViewResolver();
+	}
+
+	// forward requests for static resources to the servlet container’s default
+	// servlet
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
 	}
--- spittr/src/main/webapp/WEB-INF/views/home.jsp
+++ spittr/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,17 +1,6 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1" isELIgnored="false"  %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-<%@ taglib uri="http://www.springframework.org/tags" prefix="s"%>
 <%@ page session="false"%>
-<html>
-<head>
-<title>Spittr</title>
-<link rel="stylesheet" type="text/css"
-	href="<c:url value="/resources/style.css" />">
-</head>
-<body>
-	<h1><s:message code="spittr.welcome"/></h1>
-	<a href="<c:url value="/spittles" />">Spittles</a> |
-	<a href="<c:url value="/spitter/register" />"><s:message code="spittr.register" /></a>
-</body>
-</html>
+<h1>Welcome to Spittr</h1>
+<a href="<c:url value="/spittles" />">Spittles</a>
+|
+<a href="<c:url value="/spitter/register" />">Register</a>
