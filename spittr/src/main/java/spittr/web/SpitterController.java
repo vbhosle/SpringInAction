7a1c5a3d96a3265a5962405a5bea15e37a4997b3
Viraj Bhosle <bhosleviraj@gmail.com>
2018-12-10 16:57:50 +0530

    flashAttributes: Carrying data accross redirect requests

--- spittr/src/main/java/spittr/web/SpitterController.java
+++ spittr/src/main/java/spittr/web/SpitterController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import spittr.Spitter;
 import spittr.data.SpitterRepository;
@@ -36,22 +37,31 @@ public class SpitterController {
 	@RequestMapping(value="register", method= RequestMethod.POST)
 	public String processRegistration(
 				@RequestPart("profilePicture") MultipartFile profilePicture,
-				@Valid Spitter spitter, 
+				@Valid Spitter spitter,
+				RedirectAttributes model,
 				Errors errors
 	 ) throws IllegalStateException, IOException {
 		if(errors.hasErrors()) {
 			return "registerForm";
 		}
 		spitterRepository.save(spitter);
-		profilePicture.transferTo(
-				new File("/data/spittr/" + profilePicture.getOriginalFilename()));
-		return "redirect:/spitter/" + spitter.getUsername();
+		if (profilePicture.getSize() > 0) {
+			System.out.println("ProfilePicture.getSize " + profilePicture.getSize());
+			profilePicture.transferTo(
+					new File("/data/spittr/" + profilePicture.getOriginalFilename()));
+		}
+		
+		model.addAttribute("username", spitter.getUsername());
+		model.addFlashAttribute("spitter", spitter);
+		return "redirect:/spitter/{username}";
 	}
 	
 	@RequestMapping(value="/{username}", method=RequestMethod.GET)
 	public String showSpitterProfile(@PathVariable("username") String username, Model model){
-		Spitter spitter = spitterRepository.findByUsername(username);
-		model.addAttribute(spitter);
+		if(!model.containsAttribute("spitter")) {
+			Spitter spitter = spitterRepository.findByUsername(username);
+			model.addAttribute(spitter);
+		}
 		return "profile";
 	}
 }
